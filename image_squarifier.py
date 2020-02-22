824b88f6ee6d2632e2385d1733703c8c20f066bd
Ranjitha Gurushanthappa <ranjitha.gurushanth@gmail.com>
2020-02-21 20:12:19 -0500

    Update program for image squarifier

--- image_squarifier.py
+++ image_squarifier.py
@@ -1,21 +1,38 @@
 from PIL import Image
 
-def make_square(im, min_size=256):
+def stretch(im, min_size=256):
     x, y = im.size
     size = max(min_size, x, y)
     new_im = im.resize((size,size))
     return new_im
+
+def transparent_padding(im, min_size=256,fill_color = (0,0, 0,0) ):
+    x, y = im.size
+    size = max(min_size, x, y)
+    new_im = Image.new('RGBA', (size, size), fill_color)
+    new_im.paste(im, (int((size - x) / 2), int((size - y) / 2)))
+    return new_im
+
+def generate_new_image(image):
+    try:
+        test_image = Image.open(image)
+        choice = input("Would you like to stretch the image or add transparent padding?\n(Type s for stretch and p for transparent padding): ")
+        while(choice.lower() != "s" and choice.lower() != "p"):
+            print("Didn't enter s or p. Please choose the right option.")
+            choice = input("Would you like to stretch the image or add transparent padding?\n(Type s for stretch and p for transparent padding): ")
+        if (choice.lower() == "s"):
+            new_image = stretch(test_image)
+        elif (choice.lower() == "p"):
+            new_image = transparent_padding(test_image)
+        new_image.show()
+        new_image.save(new_image_name)
+    except:
+        raise OSError("Improper Path")
 
 if __name__=="__main__":
     done = False
     while(done == False):
         image = input("Please enter a path to an image: ")
         new_image_name = input("Enter new file name for scaled image: ")
-        try:
-            test_image = Image.open(image)
-            new_image = make_square(test_image)
-            new_image.show()
-            new_image.save("new_image_name")
-            done = True
-        except:
-            raise OSError("Improper Path")
+        generate_new_image(image)
+        done = True
